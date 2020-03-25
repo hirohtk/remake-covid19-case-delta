3031ac5f926b7bf1ffa572c90880a109ad61e42d
Kensen <hirohtk@gmail.com>
2020-03-24 21:31:31 -0700

    had to also change my updater to search by keyId instead of province, since now there are multiple entries by a single 'province'/state

--- routes/routes.js
+++ routes/routes.js
@@ -35,18 +35,18 @@ router.get("/update", function (req, res) {
     let data = response.data.data.covid19Stats
 
     for (let i = 0; i < data.length; i++) {
-      db.USA.findOne({ province: data[i].province }).then((response) => {
+      db.USA.findOne({ keyId: data[i].keyId }).then((response) => {
         if (response.lastUpdate[response.lastUpdate.length - 1] === data[i].lastUpdate) {
           // skip updating
-          console.log(`No new data.  Skipping update for ${data[i].province}`);
+          console.log(`No new data.  Skipping update for ${data[i].keyId}`);
         }
         else {
           //update with new data
           db.USA.findOneAndUpdate(
-            { province: data[i].province },
+            { keyId: data[i].keyId },
             { $push: { lastUpdate: data[i].lastUpdate, confirmed: data[i].confirmed, deaths: data[i].deaths, recovered: data[i].recovered } }
           ).then((response) => {
-            console.log(`New data found for ${data[i].province}`);
+            console.log(`New data found for ${data[i].keyId}`);
             console.log(response);
           })
         }
